cae711d80729fb39da01e1e0fee800b572f762bf
ASLawan <nawalnitsua@gmail.com>
2024-06-06 21:25:34 +0100

    Implement a function to solve the lockboxes problem

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+    Module with a function that implements lockboxes algorithm
+    checks if a given list of boxes can all be unlocked or not
+"""
+
+
+def canUnlockAll(boxes):
+    """Checks if all the boxes can be unlocked or not"""
+    n = len(boxes)
+
+    unlocked = [False] * n
+
+    unlocked[0] = True
+
+    keys = [0]
+
+    while keys:
+
+        current_key = keys.pop()
+
+        for key in boxes[current_key]:
+
+            if key < n and not  unlocked[key]:
+
+                unlocked[key] = True
+
+                keys.append(key)
+
+    return all(unlocked)
